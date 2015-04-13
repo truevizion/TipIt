473579ae1d8e0783a311108c13be768cc4cddc76
truevizion <dmarketingsolutions@gmail.com>
2015-04-12 20:15:19 -0700

    add .gitignore file
    # Xcode
    #
    build/
    *.pbxuser
    !default.pbxuser
    *.mode1v3
    !default.mode1v3
    *.mode2v3
    !default.mode2v3
    *.perspectivev3
    !default.perspectivev3
    xcuserdata
    *.xccheckout
    *.moved-aside
    DerivedData
    *.hmap
    *.ipa
    *.xcuserstate
    
    # CocoaPods
    #
    # We recommend against adding the Pods directory to your .gitignore.
    However
    # you should judge for yourself, the pros and cons are mentioned at:
    #
    http://guides.cocoapods.org/using/using-cocoapods.html#should-i-ignore-t
    he-pods-directory-in-source-control
    #
    Pods/

--- TipIt/ViewController.swift
+++ TipIt/ViewController.swift
@@ -8,18 +8,90 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var split: UITextField!
+    
+    @IBOutlet weak var splitTotal: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var totalBillLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        splitTotal.text = "$0.00"
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func splitBill(sender: AnyObject) {
+        
+        var splitNumber = NSString(string: split.text).doubleValue
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * 0.2
+        var total = billAmount + tip
+        
+        var splitTot = total/splitNumber
+        splitTotal.text = "$\(splitTotal)"
+        
+        splitTotal.text = String(format: "$%.2f",total/splitNumber)
+        
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            self.tipLabel.alpha = 0.3
+            self.tipControl.alpha = 0.3
+        })
+    }
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+       println("User editing bill")
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+    
+    @IBAction func onTap(sender: AnyObject) {
+        
+        view.endEditing (true)
+        
+        println(brandNewString("frank"))
+    }
+   
+    func brandNewString(name: String) -> String {
+        return "jdjjfdj: \(name)"
+    }
 }
 
